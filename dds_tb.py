e8825a69080e5f71251c18bf678af101c2a7c34b
aleksandaR <arsovic.aleksandar@gmail.com>
2020-02-10 04:24:28 +0100

    Sve je namesteno kako treba

--- dds_tb.py
+++ dds_tb.py
@@ -6,16 +6,18 @@ import math
 import numpy as np
 import matplotlib.pyplot as plt
 from scipy.signal import *
+from matplotlib import rc
+rc('text', usetex=True)
 
 # https://github.com/rwpenney/spfpm/blob/master/FixedPoint.py
 import FixedPoint as fp
 
-NO_BITS_DAC = 14
-NO_CORDIC_IT = 5 # ?
-NO_BITS_PHASE_ACC = 12     # M = W 
-NO_SAMPLES = 8096
+NO_BITS_DAC = 14 
+NO_CORDIC_IT = 12
+NO_BITS_PHASE_ACC = 40     # M = W 
+NO_SAMPLES = 4096
 SAMPLING_FREQ = 100e6 # Hz
-UPSAMPLE = 60
+UPSAMPLE = 40
 FIR_FILTER_ORDER = 7
 
 # fixed float point function
@@ -29,7 +31,7 @@ def float_to_precision(number, no_bits_fraction, no_bits_int):
 # D Grujic quanizator ADC class
 
 class quantizator():
-    def __init__(self, nbits, offset = 0.5):
+    def __init__(self, nbits, offset = 0):
         self.nbits = nbits
         self.offset = offset
         self.levels = -2**(nbits-1)+1.0*np.arange(0,2**nbits+1,1)+offset
@@ -99,15 +101,13 @@ def CORDIC(phi):
 def generate_phase(f0):
     phi0 = np.zeros(NO_SAMPLES)
     step = float_to_precision(f0 / SAMPLING_FREQ *2 , NO_BITS_PHASE_ACC - 3,3) 
-    # 2 times smaller sampling freq cause sth
-    noise = np.random.uniform(0, 0.0000005,NO_SAMPLES)
-    # phi0 += noise
+    noise = np.random.normal(0, 0.00005,NO_SAMPLES) 
+    # Dithering noise in phase acc is off
+    #phi0 += noise
     array_precision = []
     for i in (np.cumsum([step] * NO_SAMPLES + phi0)%2-1)*math.pi :
         array_precision.append(float_to_precision(i, NO_BITS_PHASE_ACC - 3,3))
-
     return array_precision
-    # return (np.cumsum([step] * NO_SAMPLES + phi0)%2-1)*math.pi
 
 # ================================================================================================================================================
 
@@ -137,16 +137,14 @@ def FFT_upsampled(val):    # upsampled
 
 def DAC_nrz(samples, upsample):
     quant = quantizator(NO_BITS_DAC)
-    # quant.stepErrors(0.12)
     new_samples = np.array([samples] * upsample).transpose().flatten()
-    dither = 0 * np.random.uniform(0,quant.delta**2/12,NO_SAMPLES*UPSAMPLE)
-    new_samples = quant(new_samples+dither)
+    dither = 1 * np.random.normal(0,0.05,NO_SAMPLES*UPSAMPLE)
+    #new_samples = quant(new_samples+dither)
     time = np.cumsum([1.0/SAMPLING_FREQ/upsample] * (len(samples)*upsample))
     return (new_samples, time)
 
 def DAC_brz(samples, upsample):
     quant = quantizator(NO_BITS_DAC)
-    # quant.stepErrors(0.12)
 
     upsample1 = int(upsample/4)
     upsample2 = int(upsample/2)
@@ -155,7 +153,7 @@ def DAC_brz(samples, upsample):
     new_samples = np.array([samples]*upsample1 + [-samples]*upsample2 +
                            [0*samples]*upsample3)
     new_samples = new_samples.transpose().flatten()
-    dither = 0 * np.random.uniform(0,quant.delta**2/12,NO_SAMPLES*UPSAMPLE)
+    dither = 0 * np.random.normal(0,0.05,NO_SAMPLES*UPSAMPLE)
     new_samples = quant(new_samples+dither)
     time = np.cumsum([1.0/SAMPLING_FREQ/upsample] * (len(samples) * upsample))
     return (new_samples, time)
@@ -167,11 +165,10 @@ def DAC_brz(samples, upsample):
 def DAC_nrz_fp(samples, upsample):
     
     samples_transposed = np.array([samples] * upsample).transpose().flatten()
-    # dither = 0 * np.random.uniform(0, 0.0000005,NO_SAMPLES*UPSAMPLE)
-    dither = 0 * np.random.uniform(0,0.0000005);
+    dither = 0 * np.random.normal(0,0.15,NO_SAMPLES*UPSAMPLE)
     samples_fp = []
-    for sample in samples_transposed:
-        samples_fp.append(float_to_precision(sample+dither, NO_BITS_DAC-2,2))
+    for i in range(0,len(samples_transposed)):
+        samples_fp.append(float_to_precision(samples_transposed[i]+dither[i], NO_BITS_DAC-2,3))
     time = np.cumsum([1.0/SAMPLING_FREQ/upsample] * (len(samples)*upsample))
     return (samples_fp, time)
 
@@ -184,11 +181,10 @@ def DAC_brz_fp(samples, upsample):
     new_samples = np.array([samples]*upsample1 + [-samples]*upsample2 +
                            [0*samples]*upsample3)
     samples_transposed = new_samples.transpose().flatten()
-    # dither = 0 * np.random.uniform(0, 0.0000005,NO_SAMPLES*UPSAMPLE)
-    dither = 0 * np.random.uniform(0,0.0000005);
+    dither = 0 * np.random.normal(0,0.15,NO_SAMPLES*UPSAMPLE)
     samples_fp = []
-    for sample in samples_transposed:
-        samples_fp.append(float_to_precision(sample+dither, NO_BITS_DAC-2,2)) 
+    for i in range(0,len(samples_transposed)):
+        samples_fp.append(float_to_precision(samples_transposed[i]+dither[i], NO_BITS_DAC-2,3))
     time = np.cumsum([1.0/SAMPLING_FREQ/upsample] * (len(samples) * upsample))
     return (samples_fp, time)
 
@@ -202,8 +198,6 @@ def LP_cheby_2nd():
     Wa = 59e6
     b, a = cheby2(11, 60, Wn = Wa, analog=True)
     w, H = freqs(b,a, worN = np.linspace(0,100e6,100))
-    # plt.plot(w,20*np.log10(np.abs(H)))
-    # plt.show()
     return b,a
 
 def BP_cheby_2nd():
@@ -211,8 +205,6 @@ def BP_cheby_2nd():
     Wa2 = 159e6
     b, a = cheby2(8, 60, Wn = (Wa1, Wa2), btype="bandpass", analog = True)
     w, H = freqs(b, a, worN = np.linspace(60e+6,200e+6,300))
-    # plt.plot(w,20*np.log10(np.abs(H)))
-    # plt.show()
     return b,a
    
 # ================================================================================================================================================
@@ -248,10 +240,12 @@ plt.plot(w/(2*np.pi),np.absolute(H), 'gray', aa=True, linewidth=2.5,label=r'$|H(
 df = 0.005
 for i in range(0,len(goal),2):
     plt.plot((f_goal[i]/2-df,f_goal[i+1]/2+df),[goal[i], goal[i+1]], 'r',aa=True, linewidth=2.5)
-plt.title("Characteristics of sinc(x) and correction")
-plt.xlabel("frequency normalized [fs]" )
-plt.ylabel("amplitude [dB]")
+#plt.title("Characteristics of sinc(x) and correction")
+plt.xlabel("$F=f/f_s$" )
+plt.ylabel("$H$ [dB]")
 plt.legend(loc='upper left')
+plt.grid(True)
+plt.savefig('slike/sinx_korekcija.pdf',bbox_inches='tight')
 plt.show()
 # ================================================================================================================================================
 
@@ -261,12 +255,14 @@ H_times_Hsinc =  20*np.log10(np.absolute(H_sinc * H))
 plt.plot(w/(2*np.pi), H_times_Hsinc, label=b'characteristic')
 plt.xlim(0,0.4)
 plt.ylim(-0.1,0.1)
-plt.xlabel("frequency normalized [fs]" )
-plt.ylabel("amplitude [dB]")
-plt.title("Part of FIR filter characteristic of significance")
+plt.xlabel("$F=f/f_s$" )
+plt.ylabel("$H$ [dB]")
+#plt.title("Part of FIR filter characteristic of significance")
 plt.plot([0,w[-1]], [0.05, 0.05], 'r', label=r'limits')
 plt.plot([0,w[-1]], [-0.05, -0.05], 'r')
 plt.legend(loc='upper left')
+plt.grid(True)
+plt.savefig('slike/varijacija_amplitude_fir.pdf',bbox_inches='tight')
 plt.show()
 # ================================================================================================================================================
 
@@ -275,16 +271,17 @@ plt.show()
 # ================================================================================================================================================
 # main function
 
-
-signal_freq = 17.3e6
+signal_freq = 10e6
 generated_phase = generate_phase(signal_freq)
 
 # ================================================================================================================================================
 plt.plot(generated_phase)
-plt.title("Output of phase accumulator")
-plt.xlabel("time [s]")
-plt.ylabel("angle [rad]")
+#plt.title("Output of phase accumulator")
+plt.xlabel("$t$ [s]")
+plt.ylabel("$\\phi$ [rad]")
 plt.xlim(0,100)
+plt.grid(True)
+plt.savefig('slike/fazni_akumulator_izlaz.pdf',bbox_inches='tight')
 plt.show()
 # ================================================================================================================================================
 
@@ -294,10 +291,12 @@ for i in generated_phase:
 
 # ================================================================================================================================================
 plt.plot(cordic_sine)
-plt.title("Sine generated by CORDIC algorithm")
-plt.ylabel("amplitude normalized");
-plt.xlabel("time [s]")
+#plt.title("Sine generated by CORDIC algorithm")
+#plt.ylabel("");
+plt.xlabel("$t$ [s]")
 plt.xlim(0,100)
+plt.grid(True)
+plt.savefig('slike/cordic_sample.pdf',bbox_inches='tight')
 plt.show()
 # ================================================================================================================================================
 
@@ -307,23 +306,28 @@ my_spec_db = 20 * np.log10(np.abs(my_spec) / len(cordic_sine) * 2)
 # ================================================================================================================================================
 plt.plot(my_freqs/SAMPLING_FREQ, my_spec_db, label=b'spectrum')
 plt.ylim(-80,10)
-plt.title("Spectrum of CORDIC sine")
-plt.ylabel("amplitude [dB]")
-plt.xlabel("frequency normalized [fs]")
-plt.plot([0,my_freqs[-1]/SAMPLING_FREQ], [-35, -35], 'r',label=r'referent lvl')
+#plt.title("Spectrum of CORDIC sine")
+plt.ylabel("$H$ [dB]")
+plt.xlabel("$F=f/f_s$" )
+plt.plot([0,my_freqs[-1]/SAMPLING_FREQ], [-35, -35], 'r',label=r'referent')
 plt.legend(loc='upper left')
+plt.grid(True)
+plt.savefig('slike/cordic_fft.pdf',bbox_inches='tight')
 plt.show()
 # ================================================================================================================================================
 
 cordic_sine_through_FIR = np.convolve(cordic_sine,h_invsinc,mode='same')
-analog, time = DAC_nrz_fp(cordic_sine_through_FIR, UPSAMPLE)
+analog, time = DAC_nrz_fp(cordic_sine_through_FIR/1.2, UPSAMPLE)
 
 # ================================================================================================================================================
 plt.plot(time*1e6, analog)
-plt.title("Output of DA convertor nrz")
+#plt.title("Output of DA convertor nrz")
 plt.xlim(0,1)
-plt.ylabel("amplitude normalized")
-plt.xlabel("time [us]")
+plt.ylabel("$H$")
+plt.xlabel("$t$ [us]")
+plt.savefig('slike/output_da_nrz.pdf')
+plt.grid(True)
+plt.savefig('slike/da_sample_no_filter.pdf',bbox_inches='tight')
 plt.show()
 # ================================================================================================================================================
 
@@ -333,37 +337,44 @@ w_lp,H_lp = freqs(b, a, worN = fft_freqs)
 
 # ================================================================================================================================================
 plt.plot(w_lp/1e6,20*np.log10(np.abs(H_lp)))
-plt.title("Transfer characteristic of LP filter")
-plt.ylabel("amplitude [dB]")
-plt.xlabel("frequency [MHz]")
+#plt.title("Transfer characteristic of LP filter")
+plt.ylabel("$H$ [dB]")
+plt.xlabel("$f$ [MHz]")
 plt.axvline(x=40, color='b', linestyle='--')
 plt.axvline(x=60, color='r', linestyle='--')
 plt.xlim(0,300)
+plt.grid(True)
+plt.savefig('slike/lp_fft.pdf',bbox_inches='tight')
 plt.show()
 # ================================================================================================================================================
 
 filtered = fft_vals*H_lp
+t = np.arange(0, 1000*NO_SAMPLES/SAMPLING_FREQ, 1000*2/SAMPLING_FREQ/UPSAMPLE)
 
 # ================================================================================================================================================
 plt.plot(fft_freqs/1e6,20*np.log10(np.abs(filtered)/len(filtered)), label=b'spectrum')
 plt.xlim(0,250)
 plt.ylim(-200, 5)
-plt.title("Spectrum of LP filtered output of DA convertor nrz")
-plt.ylabel("amplitude [dB]")
-plt.xlabel("frequency [MHz]")
+#plt.title("Spectrum of LP filtered output of DA convertor nrz")
 plt.axvline(x=50, color='k', linestyle='--')
 plt.plot([0,fft_freqs[-1]], [-60, -60], 'r', label=r'suppression lvl needed')
+plt.ylabel("$H$ [dB]")
+plt.xlabel("$f$ [MHz]")
 plt.legend(loc='upper right')
+plt.grid(True)
+plt.savefig('slike/da_fft.pdf',bbox_inches='tight')
 plt.show()
 # ================================================================================================================================================
 
 
 # ================================================================================================================================================
-plt.plot(np.fft.ifft(filtered))
-plt.xlim(0,1000)
-plt.title("Filtered output of DA convertor nrz")
-plt.ylabel("amplitude")
-plt.xlabel("samples?")
+plt.plot(t, np.fft.ifft(filtered))
+plt.xlim(0.0005,0.001)
+#plt.title("Filtered output of DA convertor nrz")
+plt.ylabel("$v$")
+plt.xlabel("$t$ [us] ")
+plt.grid(True)
+plt.savefig('slike/da_ifft.pdf',bbox_inches='tight')
 plt.show()
 # ================================================================================================================================================
 
@@ -372,28 +383,34 @@ plt.show()
 # bipolar return to zero
 
 analog, time = DAC_brz_fp(cordic_sine, UPSAMPLE)
+
 # ================================================================================================================================================
 plt.plot(time*1e6, analog)
-plt.title("Output of raw DA convertor brz")
-plt.ylabel("amplitude normalized")
+#plt.title("Output of raw DA convertor brz")
+plt.ylabel("$H$")
 plt.xlim(0,1)
-plt.xlabel("time [s]")
+plt.xlabel("$t$ [s]")
+plt.grid(True)
+plt.savefig('slike/da_brz_sample.pdf',bbox_inches='tight')
 plt.show()
 # ================================================================================================================================================
 
 fft_freqs, fft_mag = FFT_upsampled(analog)
 b, a = BP_cheby_2nd()
 w_bp,H_bp = freqs(b,a,worN = fft_freqs)
+
 # ================================================================================================================================================
 plt.plot(w_bp/1e6,20*np.log10(np.abs(H_bp)))
-plt.title("Transfer characteristic of BP filter")
-plt.ylabel("amplitude [dB]")
-plt.xlabel("frequency [MHz]")
+#plt.title("Transfer characteristic of BP filter")
+plt.ylabel("$H$ [dB]")
+plt.xlabel("$f$ [MHz]")
 plt.xlim(0,300)
 plt.axvline(x=90, color='r', linestyle='--')
 plt.axvline(x=100, color='b', linestyle='--')
 plt.axvline(x=140, color='b', linestyle='--')
 plt.axvline(x=160, color='r', linestyle='--')
+plt.grid(True)
+plt.savefig('slike/bp_fft.pdf',bbox_inches='tight')
 plt.show()
 # ================================================================================================================================================
 
@@ -403,22 +420,26 @@ filtered = fft_mag*H_bp
 plt.plot(fft_freqs/1e6,20*np.log10(np.abs(filtered)/len(filtered)),label=b'spectrum')
 plt.xlim(0,250)
 plt.ylim(-200, 5)
-plt.title("Spectrum of BP filtered DA convertor brz")
-plt.ylabel("amplitude [dB]")
-plt.xlabel("frequency [MHz]")
+#plt.title("Spectrum of BP filtered DA convertor brz")
+plt.ylabel("$H$ [dB]")
+plt.xlabel("$f$ [MHz]")
 plt.axvline(x=100, color='k', linestyle='--')
 plt.axvline(x=150, color='k', linestyle='--')
 plt.plot([0,fft_freqs[-1]], [-60, -60], 'r', label=r'suppression lvl needed')
 plt.legend(loc='upper left')
+plt.grid(True)
+plt.savefig('slike/da_brz_fft.pdf',bbox_inches='tight')
 plt.show()
 # ================================================================================================================================================
 
 # ================================================================================================================================================
 plt.plot(np.fft.ifft(filtered))
-plt.title("Filtered DA convertor brz")
-plt.ylabel("amplitude normalized")
-plt.xlabel("samples? ")
+#plt.title("Filtered DA convertor brz")
+plt.ylabel("$v$")
+plt.xlabel("$t$ [us]")
 plt.xlim(0,1000)
+plt.grid(True)
+plt.savefig('slike/da_brz_ifft.pdf',bbox_inches='tight')
 plt.show()
 # ================================================================================================================================================
 
